2627da4ecffeef9f0872b66561f5572b544d5bbf
KC Barrett <kbarre123@gmail.com>
2013-12-13 16:17:34 +0000

    Added gallery page, glyphicons, adjust footer.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Bootstrap::Application.routes.draw do
   get '/contact' => 'high_voltage/pages#show', id: 'contact'
   get '/events' => 'high_voltage/pages#show', id: 'events'
   get '/press' => 'high_voltage/pages#show', id: 'press'
+  get '/gallery' => 'high_voltage/pages#show', id: 'gallery'
   get '/privacy' => 'high_voltage/pages#show', id: 'privacy'
   get '/products' => 'high_voltage/pages#show', id: 'products'
   get '/terms' => 'high_voltage/pages#show', id: 'terms'
